999bbf508605cc594963a5a16ce2f8f3578f46ec
bt64341 <bt64341@yandex.ru>
2021-07-02 13:26:09 +0400

    2

--- main.py
+++ main.py
@@ -1,45 +1,31 @@
-import np as np
 import numpy as np
 
+INPUT_DIM = 13
+OUT_DIM = 6
+H_DIM = 10
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+x = np.random.randn(INPUT_DIM)
 
+W1 = np.random.randn(INPUT_DIM, H_DIM)
+b1 = np.random.randn(H_DIM)
+W2 = np.random.randn(H_DIM, OUT_DIM)
+b2 = np.random.randn(OUT_DIM)
 
-tr_inputs = np.array([[ 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1],
-                      [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1],
-                      [0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1],
-                      [0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1]])
-tr_outputs = np.array([[312, 213, 123, 231]]).T
+def relu(t):
+    return np.maximum(t,0)
 
-# tr_inputs = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
-# tr_outputs = np.array([[0,1,1,0]]).T
+def softmax(t):
+    out = np.exp(t)
+    return out / np.sum(out)
 
-np.random.seed(1)
-sp_ts = np.random.random((12,3))-1
+def predict(x):
+    t1 = x @ W1 + b1
+    h1 = relu(t1)
+    t2 = h1 @ W2 + b2
+    z = softmax(t2)
+    return z
 
-print('случайные инициализирующие')
-print(sp_ts)
-
-# метод обратного распространения
-for i in range(20000):
-    input_layers = tr_inputs
-    outputs = sigmoid(np.dot(input_layers, sp_ts))
-
-    err = tr_outputs - outputs
-    adjustments = np.dot(input_layers.T, err * (outputs * (1 - outputs)))
-
-    sp_ts += adjustments
-
-print('after training')
-print(sp_ts)
-
-print('result')
-print(outputs)
-
-# test
-new_inputs = np.array([ 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1])
-outputs = sigmoid(np.dot(new_inputs, sp_ts))
-
-print('new situation')
-print(outputs)
+probs = predict(x)
+pred_class = np.argmax(probs)
+class_names = ['xyz','xzy','yxz','yzx','zxy','zyx']
+print(class_names[pred_class])
